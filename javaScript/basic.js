543654342b9963c2695eb2cec4aec08382a4b0dd
Muhammad Huzaifa <mhuzaifaking@gmail.com>
2023-12-03 13:08:51 +0500

    added Some code to check every thing properly

--- javaScript/basic.js
+++ javaScript/basic.js
@@ -1,5 +1,16 @@
-console.log("Hello World");
-var word = "word";
-console.log("Hello ",word);
-var isHelloWordPrints = true;
-console.log(isHelloWordPrints);
+// console.log("Hello World");
+// var word = "word";
+// console.log("Hello ",word);
+// var isHelloWordPrints = true;
+// console.log(isHelloWordPrints);
+var number;
+var type;
+number = 2;
+number%2 == 0? console.log("Number is Even"):console.log("Number is odd");
+number = 5;
+if(number%2 == 0){
+    type ="Number is Even";
+}else{
+    type = "number is odd";
+}
+console.log(type);
